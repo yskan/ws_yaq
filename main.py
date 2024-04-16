3e72e6a0f7bda9610b460504b571e18d24656af4
Youssef <youssef.skandrani@outlook.com>
2024-04-16 21:26:22 +0200

    Handle client disconnection and parse incoming data in main.py

--- main.py
+++ main.py
@@ -20,20 +20,33 @@ def echo(sock):
     while True:
         data = sock.receive()
         print(data)
-        if data == "CLICK":
+        
+        # Handle client disconnection
+        if data == "close":
+            clients.remove(sock)
+            sock.close()
+        
+        # Parse incoming data
+        try:
+            data = json.loads(data)
+        except json.JSONDecodeError:
+            err_response = { "error": "Invalid request", "tz": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') }
+            sock.send(json.dumps(err_response))
+            continue
+        
+        event_type = data.get("event_type")
+        print(event_type)
+        # Handle incoming events
+        if event_type == "click":
             global count
             count += 1
             response = { "count": count, "tz": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') }
             for s in clients:
                 s.send(json.dumps(response))
-        elif data == "close":
-            clients.remove(sock)
-            sock.close()
-        else:
-            err_response = { "error": "Invalid request", "tz": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') }
+        
+        if event_type == "bullshit":
+            err_response = { "error": "Bullshit ERROR", "tz": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') }
             sock.send(json.dumps(err_response))
-        # print(data)
-        # sock.send(data)
-
+        
 if __name__ == "__main__":
     app.run()
