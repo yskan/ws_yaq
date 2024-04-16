3c51e7d0fe0797011b39a005086e8d0a10efb439
Youssef <youssef.skandrani@outlook.com>
2024-04-16 22:22:58 +0200

    Refactor main.py to handle client disconnection and parse incoming data

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from flask import Flask, render_template
 from flask_sock import Sock
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key'
+app.config['SOCK_SERVER_OPTIONS'] = {'ping_interval': 25}
 sock = Sock(app)
 clients = []
+clients_map = {}
+reversed_clients_map = {}
 
 count = 0
 
@@ -16,14 +18,17 @@ def index():
 
 @sock.route('/ws')
 def echo(sock):
-    clients.append(sock)
     while True:
         data = sock.receive()
-        print(data)
-        
+        print("Data RECV: ", data)
+
         # Handle client disconnection
         if data == "close":
             clients.remove(sock)
+            username = reversed_clients_map[sock]
+            print("User disconnected: ", username)
+            del reversed_clients_map[sock]
+            del clients_map[username]
             sock.close()
         
         # Parse incoming data
@@ -35,13 +40,31 @@ def echo(sock):
             continue
         
         event_type = data.get("event_type")
-        print(event_type)
+        print("Event_type: ", event_type)
+
         # Handle incoming events
+        if event_type == "delete_me":
+            username = reversed_clients_map[sock]
+            print("User disconnected: ", username)
+            del reversed_clients_map[sock]
+            del clients_map[username]
+            sock.close()
+
+        if event_type == "register_username":
+            username = data.get("username")
+            clients_map[username] = sock
+            reversed_clients_map[sock] = username
+            print(clients_map)
+            for u, s in clients_map.items():
+                print("Sending new client event to: ", u)
+                s.send(json.dumps({ "event_type": "new_client", "username": username}))
+
         if event_type == "click":
             global count
             count += 1
-            response = { "count": count, "tz": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') }
-            for s in clients:
+            sender_username = reversed_clients_map[sock]
+            response = { "username": sender_username, "count": count, "tz": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') }
+            for s in clients_map.values():
                 s.send(json.dumps(response))
         
         if event_type == "bullshit":
